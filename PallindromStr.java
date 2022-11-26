70bdeb8769796ba1be89806530b4add96d618946
Sauravkumar1232 <kumarsaurav11494@gmail.com>
2022-11-27 00:05:55 +0530

    new

---
+++ PallindromStr.java
@@ -0,0 +1,56 @@
+public class PallindromStr {
+    // static boolean reverce(String name, int i, String newstr) {
+    // if (name.length() == i) {
+    // System.out.println(newstr);
+    // // if(str==newstr){
+    // // System.out.println("yes");;
+    // // }
+    // // else System.out.println("no");;
+    // return true;
+    // }
+    // char currentchar = name.charAt(i);
+
+    // newstr = currentchar + newstr;
+    // return reverce(name, i + 1, newstr);
+
+    // }
+
+    // static boolean isPallindrom(String str, String newstr) {
+    // if (str.equals(newstr)) {
+    // return true;
+    // } else
+    // return false;
+    // }
+
+    // public static void main(String[] args) {
+    // String str = ""saurav";
+    // boolean isPallindrom=checkPallindrom(";
+    // int i = 0;
+    // String newstr = "";
+    // boolean isPallindrom = reverce(str,i, "");
+    // if (isPallindrom) {
+
+    // System.out.println("yes");
+    // } else
+    // System.out.println("no");
+
+    // }
+    public static void main(String[] args) {
+        String name = "aaabaaa";
+        boolean isPallindrom = checkPallindrom(name, 0, name.length() - 1);
+        if (isPallindrom) {
+            System.out.println("yes");
+        } else
+            System.out.println("no");
+    }
+
+    private static boolean checkPallindrom(String str, int i, int j) {
+        if (i > j) {
+            return true;
+        }
+        if (str.charAt(i) != str.charAt(j)) {
+            return false;
+        } else
+            return checkPallindrom(str, i + 1, j - 1);
+    }
+}
--- ReverceString.java
+++ ReverceString.java
@@ -1,23 +1,35 @@
 public class ReverceString {
-    static String reverce(String str,String newstr,int i){
-        if(str.length()==i){
-            return newstr ;
-        }
+    // static String reverce(String str,String newstr,int i){
+    //     if(str.length()==i){
+    //         return newstr ;
+    //     }
         
-        char currentchar=str.charAt(i);
-        System.out.println(currentchar);
-        
-        
-        return  
-        reverce(str, currentchar+newstr,i+1);
+    //     char currentchar=str.charAt(i);
+    //     System.out.println(currentchar);
         
         
-    }
+    //     return  
+    //     reverce(str, currentchar+newstr,i+1);
+        
+        
+    // }
+    // public static void main(String[] args) {
+    //     String str="abcd";
+    //     //int size=str.length();
+    //     String newstr="";
+    //     int i=0;
+    //    System.out.println(reverce(str,newstr,i)); 
+    // }
     public static void main(String[] args) {
         String str="abcd";
-        //int size=str.length();
-        String newstr="";
         int i=0;
-       System.out.println(reverce(str,newstr,i)); 
+        int j=str.length();
+        reverce(str,i,j);
+    }
+    static void reverce(String str,int i,int j){
+        if(i>j){
+            return;
+        }
+        swap()
     }
 }
