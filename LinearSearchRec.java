8e5c0d0eaf6bbdfec4f33a45d6acc403b928de3d
Sauravkumar1232 <97805415+Sauravkumar1232@users.noreply.github.com>
2022-12-13 22:32:13 +0530

    Update LinearSearchRec.java

--- LinearSearchRec.java
+++ LinearSearchRec.java
@@ -65,7 +65,7 @@ public class LinearSearchRec {
         if (arr[index] == target) {
             list.add(index);
         }
-         return findallindexces(arr, target, index + 1, list);    here list variables are different reference variables although they have same name
+         return findallindexces(arr, target, index + 1, list);    //here list variables are different reference variables although they have same name
     }
 
 }
